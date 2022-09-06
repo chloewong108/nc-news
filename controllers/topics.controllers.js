6fdc30e51d6400f52ab65d1dd6a1d7deb1af4917
chloewong108 <chloewng@ymail.com>
2022-09-06 11:51:26 +0100

    ticket 4 completed

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,7 +1,18 @@
-const { selectTopics } = require("../models/topics.models");
+const { selectTopics, selectArticleId } = require("../models/topics.models");
 
 exports.getTopics = (req, res) => {
   selectTopics().then((topics) => {
     res.status(200).send({ topics: topics });
   });
+};
+
+exports.getArticleId = (req, res, next) => {
+  const { article_id } = req.params;
+  selectArticleId(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
--- models/topics.models.js
+++ models/topics.models.js
@@ -6,3 +6,20 @@ exports.selectTopics = () => {
     return response.rows;
   });
 };
+
+exports.selectArticleId = (id) => {
+  return db
+    .query(
+      `SELECT articles.* FROM articles JOIN users ON users.username = articles.author WHERE articles.article_id = $1;`,
+      [id]
+    )
+    .then(({ rows: [response] }) => {
+      if (!response) {
+        return Promise.reject({
+          status: 404,
+          msg: "error 404: does not exist.",
+        });
+      }
+      return response;
+    });
+};
