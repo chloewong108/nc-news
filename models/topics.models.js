8ba8fce61ab36410e80506d6fe1b8cd4b2ff2815
chloewong108 <chloewng@ymail.com>
2022-09-06 16:25:11 +0100

    ticket 6 completed

--- models/topics.models.js
+++ models/topics.models.js
@@ -27,3 +27,17 @@ exports.selectUsers = () => {
     return response.rows;
   });
 };
+
+exports.updatedArticle = (id, votes) => {
+  const query = `UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *`;
+  return db.query(query, [votes, id]).then((response) => {
+    console.log(response);
+    if (response.rows.length === 0) {
+      return Promise.reject({
+        status: 404,
+        msg: "error 404: does not exist",
+      });
+    }
+    return response.rows[0];
+  });
+};
