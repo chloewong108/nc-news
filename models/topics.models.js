0747c1140d013e7d1b495d79a54757aacb825575
chloewong108 <chloewng@ymail.com>
2022-09-06 16:26:17 +0100

    updated models ticket 6 completed

--- models/topics.models.js
+++ models/topics.models.js
@@ -31,7 +31,6 @@ exports.selectUsers = () => {
 exports.updatedArticle = (id, votes) => {
   const query = `UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *`;
   return db.query(query, [votes, id]).then((response) => {
-    console.log(response);
     if (response.rows.length === 0) {
       return Promise.reject({
         status: 404,
