481fb6d0a676a88f71498e61c1b1d4cd43f77219
chloewong108 <chloewng@ymail.com>
2022-09-06 11:50:57 +0100

    ticket 4 completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,7 +12,7 @@ afterAll(() => {
   return db.end();
 });
 
-describe.only("/api/topics", () => {
+describe("/api/topics", () => {
   describe("GET", () => {
     test("200: Responds with array of topic objects with the properties of slug and description", () => {
       return request(app)
@@ -25,6 +25,43 @@ describe.only("/api/topics", () => {
             expect(topic).toHaveProperty("description", expect.any(String));
             expect(topic).toHaveProperty("slug", expect.any(String));
           });
+        });
+    });
+  });
+});
+
+describe("/api/articles/:article_id", () => {
+  describe("GET", () => {
+    test("200: Responds with an article object with its properties", () => {
+      return request(app)
+        .get(`/api/articles/1`)
+        .expect(200)
+        .then(({ body }) => {
+          expect(body.article).toEqual({
+            article_id: 1,
+            title: "Living in the shadow of a great man",
+            topic: "mitch",
+            author: "butter_bridge",
+            body: "I find this existence challenging",
+            created_at: expect.any(String),
+            votes: 100,
+          });
+        });
+    });
+    test("404: Responds with does not exist if article_id does not exist", () => {
+      return request(app)
+        .get("/api/articles/1000000")
+        .expect(404)
+        .then(({ body }) => {
+          expect(body.msg).toBe("error 404: does not exist.");
+        });
+    });
+    test("400: Responds with bad request if article_id is invalid", () => {
+      return request(app)
+        .get("/api/articles/apple")
+        .expect(400)
+        .then(({ body }) => {
+          expect(body.msg).toBe("error 400: bad request.");
         });
     });
   });
